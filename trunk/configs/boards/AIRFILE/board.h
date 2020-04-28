fe1d4c1108179e61d36d2ec85b533839bd9445cd
exition <exition@msn.com>
2020-04-28 16:13:49 +0800

    Update board.h

--- trunk/configs/boards/AIRFILE/board.h
+++ trunk/configs/boards/AIRFILE/board.h
@@ -1,11 +1,11 @@
 /* HC5661A */
 
-define BOARD_PID		"AIRFILE"
-define BOARD_NAME		"AIRFILE"
-define BOARD_DESC		"AIRFILE Wireless TFcard Reader"
-define BOARD_VENDOR_NAME	"AIRFILE"
-define BOARD_VENDOR_URL	"http://www.AIRFILE.com/"
-define BOARD_MODEL_URL		"http://www.AIRFILE.com/"
+#define BOARD_PID		"AIRFILE"
+#define BOARD_NAME		"AIRFILE"
+#define BOARD_DESC		"AIRFILE Wireless TFcard Reader"
+#define BOARD_VENDOR_NAME	"AIRFILE"
+#define BOARD_VENDOR_URL	"http://www.AIRFILE.com/"
+#define BOARD_MODEL_URL		"http://www.AIRFILE.com/"
 #define BOARD_BOOT_TIME		25
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	38
@@ -23,8 +23,8 @@ define BOARD_MODEL_URL		"http://www.AIRFILE.com/"
 #define BOARD_HAS_5G_11AC	0
 #define BOARD_NUM_ANT_5G_TX	0
 #define BOARD_NUM_ANT_5G_RX	0
-define BOARD_NUM_ANT_2G_TX	1
-define BOARD_NUM_ANT_2G_RX	1
+#define BOARD_NUM_ANT_2G_TX	1
+#define BOARD_NUM_ANT_2G_RX	1
 #define BOARD_NUM_ETH_LEDS	0
 #define BOARD_HAS_EPHY_L1000	0
 #define BOARD_HAS_EPHY_W1000	0
